592b4ed7fcf52bc3e2bf6223fa621ba4b8b35a67
GithubKangMin <leelucky1@naver.com>
2024-11-12 10:11:25 +0900

    code

--- 1주차 워크북/region.sql
+++
@@ -1,9 +0,0 @@
-create table region
-(
-    name       varchar(6)  null,
-    created_at datetime(6) null,
-    updated_at datetime(6) null,
-    id         bigint auto_increment
-        primary key
-);
-
--- 2주차 워크북/review.sql
+++
@@ -1,3 +0,0 @@
-
-INSERT INTO review (content, rating, store_id, created_at, updated_at)
-VALUES ('리뷰 내용', 4.5, {store_id}, NOW(), NOW());
